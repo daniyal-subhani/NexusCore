8e227f064ed2dbe588f1bb5c21700f110fcfdc33
daniyal-subhani <daniyalsubhani527@gmail.com>
2026-08-07 15:45:59 +0500

    add app in chat-service

--- nexus-core-backend/services/chat-service/src/app.ts
+++ nexus-core-backend/services/chat-service/src/app.ts
@@ -2,3 +2,15 @@ import express, {type Express } from "express";
 import helmet from 'helmet';
 import cors from "cors"
 import { conversationRouter } from "./routes/conversation.routes.js";
+import { errorHandler } from "./middleware/error-handler.js";
+
+
+export function createApp(): Express {
+    const app = express();
+    app.use(helmet());
+    app.use(cors());
+    app.use(express.json())
+    app.get('/conversations', conversationRouter);
+    app.use(errorHandler)
+    return app;
+}
