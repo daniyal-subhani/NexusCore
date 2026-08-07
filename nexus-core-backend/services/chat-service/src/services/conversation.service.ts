efc240cc053a966bbab835aa95149804b96e26ff
daniyal-subhani <daniyalsubhani527@gmail.com>
2026-08-07 14:43:02 +0500

    list for User

--- nexus-core-backend/services/chat-service/src/services/conversation.service.ts
+++ nexus-core-backend/services/chat-service/src/services/conversation.service.ts
@@ -1,15 +1,23 @@
-import { conversationCache } from "@/cache/conversation.cache.js";
-import { HttpError } from "@nexus-core/common";
-import { conversationRepository } from "@/repositories/conversation.repository.js";
+import { conversationCache } from '@/cache/conversation.cache.js';
+import { HttpError } from '@nexus-core/common';
+import { conversationRepository } from '@/repositories/conversation.repository.js';
 
 export const conversationService = {
-    async create(participantIds: string[]) {
-        if(participantIds.length < 2) {
-            throw new HttpError(400, "A conversation needs at least 2 participants");
-        }
-        const conversation = await conversationRepository.create(participantIds);
-        // dono participants ka cached list ab stale hai - invalidate karo
-        await Promise.all(participantIds.map((id) => conversationCache.invalidate(id)))
-        return conversation;
+  async create(participantIds: string[]) {
+    if (participantIds.length < 2) {
+      throw new HttpError(400, 'A conversation needs at least 2 participants');
     }
-}
+    const conversation = await conversationRepository.create(participantIds);
+    // dono participants ka cached list ab stale hai - invalidate karo
+    await Promise.all(participantIds.map((id) => conversationCache.invalidate(id)));
+    return conversation;
+  },
+  async listForUser(userId: string) {
+    const cached = await conversationCache.get(userId);
+    if (cached) return cached;
+
+    const conversations = await conversationRepository.findByPartifipant(userId);
+    await conversationCache.set(userId, conversations);
+    return conversations
+  },
+};
