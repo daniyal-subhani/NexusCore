ae95d33f10a0a9dedfb009403014b7f98ea8d1e1
daniyal-subhani <daniyalsubhani527@gmail.com>
2026-08-07 13:06:24 +0500

    conversation

---
+++ nexus-core-backend/services/chat-service/src/services/conversation.service.ts
@@ -0,0 +1,3 @@
+import { conversationCache } from "@/cache/conversation.cache.js";
+import { HttpError } from "@nexus-core/common";
+import { conversationRepository } from "@/repositories/conversation.repository.js";
