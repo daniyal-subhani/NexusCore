43a1d791cc171e9574a87d8cf4c59b40e342224d
daniyal-subhani <daniyalsubhani527@gmail.com>
2026-08-08 01:04:16 +0500

    edit env.ts

--- nexus-core-backend/services/gateway-service/src/config/env.ts
+++ nexus-core-backend/services/gateway-service/src/config/env.ts
@@ -5,7 +5,9 @@ const envSchema = z.object({
     GATEWAY_PORT: z.coerce.number().int().min(0).max(65_535).default(4000),
     NODE_ENV: z.enum(['development', 'production', 'test']).default('development'),
     AUTH_SERVICE_URL: z.url().default('http://localhost:4003'),
-    USER_SERVICE_URL: z.url().default("http://localhost:4001")
+    USER_SERVICE_URL: z.url().default("http://localhost:4001"),
+    JWT_ACCESS_SECRET: z.string().min(16),
+    CHAT_SERVICE_URL: z.url()
 })
 
 type EnvType = z.infer<typeof envSchema>;
