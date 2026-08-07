69d8835c8b0c8829ab135e8fffc7f24573e74021
daniyal-subhani <daniyalsubhani527@gmail.com>
2026-08-07 15:07:13 +0500

    conversationController(listMessages)

--- nexus-core-backend/services/chat-service/src/controllers/conversation.controller.ts
+++ nexus-core-backend/services/chat-service/src/controllers/conversation.controller.ts
@@ -1,15 +1,18 @@
-import type { Request, Response } from "express";
-import { conversationService } from "@/services/conversation.service.js";
-import { messageService } from "@/services/message.service.js";
+import type { Request, Response } from 'express';
+import { conversationService } from '@/services/conversation.service.js';
+import { messageService } from '@/services/message.service.js';
 
 export const conversationController = {
-    async create(req: Request, res:Response) {
-        const conversation = await conversationService.create(req.body.participantIds);
-        res.status(201).json(conversation)
-    },
-    async listForUser(req: Request, res:Response) {
-        const conversations = await conversationService.listForUser(req.params.userId as string)
-        res.status(201).json(conversations);
-    },
-    
-}
+  async create(req: Request, res: Response) {
+    const conversation = await conversationService.create(req.body.participantIds);
+    res.status(201).json(conversation);
+  },
+  async listForUser(req: Request, res: Response) {
+    const conversations = await conversationService.listForUser(req.params.userId as string);
+    res.status(201).json(conversations);
+  },
+  async listMessages(req: Request, res: Response) {
+    const messages = await messageService.list(req.params.id as string);
+    res.status(200).json(messages);
+  },
+};
