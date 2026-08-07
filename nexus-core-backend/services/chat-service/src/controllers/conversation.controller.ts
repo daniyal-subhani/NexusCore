8c937a1e6ea616cdcf973210bbf3a26e4886608e
daniyal-subhani <daniyalsubhani527@gmail.com>
2026-08-07 15:00:12 +0500

    conversationController

---
+++ nexus-core-backend/services/chat-service/src/controllers/conversation.controller.ts
@@ -0,0 +1,7 @@
+import type { Request, Response } from "express";
+import { conversationService } from "@/services/conversation.service.js";
+import { messageService } from "@/services/message.service.js";
+
+export const conversationController = {
+    
+}
